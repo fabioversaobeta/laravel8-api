8749b76753f3cf9b2107cd9308fd6af16b0f9918
Fabio Bandacheski <fabio.versao.beta@gmail.com>
2022-02-12 18:35:37 -0300

    phpunit init test

---
+++ tests/Feature/AccountTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class AccountTest extends TestCase
+{
+    /** @test */
+    public function reset_all_data_front_start_tests()
+    {
+        $response = $this->post('/api/reset');
+
+        $response->assertStatus(200);
+    }
+}
