adecb67684ada2ffb161c9ae9202381101ffa6fd
Fabio Bandacheski <fabio.versao.beta@gmail.com>
2022-02-13 18:32:15 -0300

    event start with request

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,3 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\AccountsController;
-use App\Http\Controllers\EventsController;
-
-Route::post('reset', [AccountsController::class, 'reset']);
-Route::post('event', [EventsController::class, 'store']);
-Route::get('balance/{account_id?}', [AccountsController::class, 'balance']);
